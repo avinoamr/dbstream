f103e45ee7199b7346360a65bd55694d627a7bb3
Roi Avinoam <avinoamr@gmail.com>
2014-07-26 01:05:49 +0300

    Simplify the API - focux on one thing, leave the functional API to different modules

--- dbstream.js
+++ dbstream.js
@@ -2,108 +2,59 @@ var stream = require( "stream" );
 var util = require( "util" );
 
 // Functional API
-util.inherits( Stream, stream.Transform );
-function Stream ( connection ) {
-    Stream.super_.call( this, { objectMode: true } );
-    this.connection = connection;
+util.inherits( Cursor, stream.Duplex );
+function Cursor () {
+    Cursor.super_.call( this, { objectMode: true } );
 }
 
-Stream.prototype.toArray = function ( fn, ctx ) {
-    var arr = [];
-    return this
-        .on( "data", function ( obj ) { arr.push( obj ) } )
-        .on( "end",  function () { fn.call( ctx || this, arr ) } );
-}
-
-Stream.prototype.filter = function ( fn, ctx ) {
-    var s = new Stream();
-    s._transform = function ( obj, encoding, callback ) {
-        if ( fn.call( ctx || this, obj ) ) this.push( obj );
-        callback();
-    }
-    return this.pipe( s );
-}
-
-Stream.prototype.map = function ( fn, ctx ) {
-    var s = new Stream();
-    s._transform = function ( obj, encoding, callback ) {
-        this.push( fn.call( ctx || this, obj ) );
-        callback();
-    }
-    return this.pipe( s );
-}
-
-Stream.prototype.reduce = function ( fn, memo, ctx ) {
-    var s = new Stream();
-    s._transform = function ( obj, encoding, callback ) {
-        memo = fn.call( ctx || this, memo, obj );
-        callback();
-    }
-    this.on( "end", function() {
-        if ( !Array.isArray( memo ) ) { memo = [ memo ] }
-        memo.forEach( s.push, s );
-    });
-    return this.pipe( s );
-}
-
-Stream.prototype.each = Stream.prototype.forEach = function ( fn, ctx ) {
-    return this.on( "data", function ( obj ) { fn.call( ctx || this, obj ) } );
-}
-
-// Database API
-util.inherits( DatabaseStream, Stream );
-function DatabaseStream ( connection ) {
-    DatabaseStream.super_.call( this, connection );
-}
-
-DatabaseStream.prototype._save = function ( obj, callback ) {
+Cursor.prototype._save = function ( obj, callback ) {
     throw new Error( "_save is not implemented" );
 }
 
-DatabaseStream.prototype._remove = function ( obj, callback ) {
+Cursor.prototype._remove = function ( obj, callback ) {
     throw new Error( "_remove is not implemented" );
 }
 
-DatabaseStream.prototype._load = function ( size ) {
+Cursor.prototype._load = function ( size ) {
     throw new Error( "_load is not implemented" );
 }
 
-DatabaseStream.prototype.remove = function ( chunk, encoding, callback ) {
+Cursor.prototype.remove = function ( chunk, encoding, callback ) {
     return this.write( { $remove: chunk }, encoding, callback );
 }
 
-DatabaseStream.prototype._read = function ( size ) {
+Cursor.prototype._read = function ( size ) {
     if ( !this._query ) return this.push( null ); // nothing to query
     return this._load( size );
 }
 
-DatabaseStream.prototype._write = function ( chunk, encoding, callback ) {
+Cursor.prototype._write = function ( chunk, encoding, callback ) {
     return ( chunk.$remove )
         ? this._remove( chunk.$remove, callback ) 
         : this._save( chunk, callback );
 }
 
 // Query API
-DatabaseStream.prototype.find = function ( query ) {
+Cursor.prototype.find = function ( query ) {
     this._query = query;
     return this;
 }
 
-DatabaseStream.prototype.sort = function ( key, direction ) {
+Cursor.prototype.sort = function ( key, direction ) {
     this._sort || ( this._sort = [] );
     this._sort.push( { key: key, direction: direction || 1 });
     return this;
 }
 
-DatabaseStream.prototype.skip = function ( n ) {
+Cursor.prototype.skip = function ( n ) {
     this._skip = n;
     return this;
 }
 
-DatabaseStream.prototype.limit = function ( n ) {
+Cursor.prototype.limit = function ( n ) {
     this._limit = n;
     return this;
 }
 
 
-module.exports.DatabaseStream = DatabaseStream;
+module.exports.Cursor = Cursor;
--- test.js
+++ test.js
@@ -4,40 +4,40 @@ var dbstream = require( "./dbstream" );
 describe( "DatabaseStream", function () {
 
     it( "Writes objects", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
+        var cursor = new dbstream.Cursor()
         var saved = [];
-        stream._save = function( object, callback ) {
+        cursor._save = function( object, callback ) {
             saved.push( object );
             callback();
         }
 
-        stream.on( "finish", function () {
+        cursor.on( "finish", function () {
             assert.equal( saved.length, 2 );
             assert.equal( saved[ 0 ].name, "Hello" );
             assert.equal( saved[ 1 ].name, "World" );
             done();
         });
-        stream.write( { name: "Hello" } );
-        stream.write( { name: "World" } );
-        stream.end();
+        cursor.write( { name: "Hello" } );
+        cursor.write( { name: "World" } );
+        cursor.end();
     });
 
     it( "Reads objects", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
+        var cursor = new dbstream.Cursor();
         var data = [
             { name: "Hello" },
             { name: "World" }
         ];
-        stream._load = function ( size ) {
+        cursor._load = function ( size ) {
             this.push( data.shift() );
         }
 
         var read = [];
-        stream.find({})
-        stream.on( "data", function ( object ) {
+        cursor.find({})
+        cursor.on( "data", function ( object ) {
             read.push( object );
         });
-        stream.on( "end", function () {
+        cursor.on( "end", function () {
             assert.equal( read.length, 2 );
             assert.equal( read[ 0 ].name, "Hello" );
             assert.equal( read[ 1 ].name, "World" );
@@ -46,28 +46,28 @@ describe( "DatabaseStream", function () {
     });
 
     it( "Removes objects", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
+        var cursor = new dbstream.Cursor();
         var removed = [];
-        stream._remove = function( chunk, callback ) {
+        cursor._remove = function( chunk, callback ) {
             removed.push( chunk );
             callback();
         }
-        stream.on( "finish", function() {
+        cursor.on( "finish", function() {
             assert.equal( removed.length, 2 );
             assert.equal( removed[ 0 ].name, "Hello" );
             assert.equal( removed[ 1 ].name, "World" );
             done();
         })
-        stream.remove({ name: "Hello" });
-        stream.remove({ name: "World" });
-        stream.end();
+        cursor.remove({ name: "Hello" });
+        cursor.remove({ name: "World" });
+        cursor.end();
     });
 
     // Query API
     it( "Provides the Query API to _load", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
+        var cursor = new dbstream.Cursor();
         var query, sort, skip, limit;
-        stream._load = function() {
+        cursor._load = function() {
             query = this._query;
             sort = this._sort;
             skip = this._skip;
@@ -77,13 +77,15 @@ describe( "DatabaseStream", function () {
             this.push( { name: "World" } );
             this.push( null );
         }
-        stream
+        var arr = [];
+        cursor
             .find( { id: 15 } )
             .sort( "name" )
             .sort( "age", -1 )
             .skip( 5 )
             .limit( 3 )
-            .toArray(function ( arr ) {
+            .on( "data", function ( obj ) { arr.push( obj ) })
+            .on( "end", function () {
                 assert.deepEqual( query, { id: 15 } );
                 assert.deepEqual( sort, [
                     { key: "name", direction: 1 },
@@ -98,100 +100,20 @@ describe( "DatabaseStream", function () {
             });
     });
 
-
-    // Functional API
-    it( "Converts to Array", function ( done ) {
-        var stream = new dbstream.DatabaseStream()
-        stream._load = function() { this.push( null ) }
-        stream.push({ name: "Hello" });
-        stream.push({ name: "World" });
-        stream.toArray( function ( arr ) {
-            assert.equal( arr.length, 2 );
-            assert.equal( arr[ 0 ].name, "Hello" );
-            assert.equal( arr[ 1 ].name, "World" );
-            done();
-        })
-    });
-
-    it( "Implements filter", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
-        stream.push( { name: "Hello" });
-        stream.push( { name: "World" });
-        stream.push( { name: "Foo" });
-
-        stream.filter(function ( obj ) {
-            return obj.name != "Foo";
-        })
-        .toArray(function ( arr ) {
-            assert.deepEqual( arr, [ { name: "Hello" }, { name: "World" } ]);
-            done();
-        })
-    });
-
-    it( "Implements each", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
-        stream.push( { name: "Hello" } );
-        stream.push( { name: "World" } );
-
-        var arr = [];
-        stream.each( function ( obj ) { arr.push( obj ) })
-        .on( "end", function () {
-            assert.deepEqual( arr, [ { name: "Hello" }, { name: "World" } ]);
-            done();
-        })
-    });
-
-    it( "Implements map", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
-        stream.push( { name: "Hello" } );
-        stream.push( { name: "World" } );
-        var c = 0;
-        stream.map( function ( obj ) {
-            obj.c = ++c;
-            return obj;
-        }).toArray( function ( arr ) {
-            assert.deepEqual( arr, [ 
-                { name: "Hello", c: 1 }, 
-                { name: "World", c: 2 } 
-            ]);
-            done();
-        })
-    });
-
-    it( "Implements reduce", function ( done ) {
-        var stream = new dbstream.DatabaseStream();
-        stream.push( { name: "Hello" } );
-        stream.push( { name: "World" } );
-        stream.reduce(function ( memo, obj ) {
-            memo.push( obj );
-            memo.push( obj );
-            return memo;
-        }, [])
-        .toArray( function ( arr ) {
-            assert.deepEqual( arr, [
-                { name: "Hello" },
-                { name: "Hello" },
-                { name: "World" },
-                { name: "World" }
-            ]);
-            done();
-        })
-    });
-
     it( "Requires implementation of _save", function() {
-        var s = new dbstream.DatabaseStream();
-        assert.throws( function() { s.write( {} ) } );
+        var cursor = new dbstream.Cursor();
+        assert.throws( function() { cursor.write( {} ) } );
     });
 
     it( "Requires implementation of _load", function() {
-        var s = new dbstream.DatabaseStream();
-        s.find({});
-        assert.throws( function() { s.read() } );
+        var cursor = new dbstream.Cursor();
+        cursor.find({});
+        assert.throws( function() { cursor.read() } );
     });
 
     it( "Requires implementation of _remove", function() {
-        var s = new dbstream.DatabaseStream();
-        assert.throws( function() { s.remove({}) } );
+        var cursor = new dbstream.Cursor();
+        assert.throws( function() { cursor.remove({}) } );
     });
 
 });
